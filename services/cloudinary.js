04ff1e837e6ff514ad438014558e14c9326106b2
UnfazedAy <ayomidesoniyi@gmail.com>
2023-08-06 22:00:19 +0100

    Loaded the env variables properly

--- services/cloudinary.js
+++ services/cloudinary.js
@@ -1,11 +1,14 @@
 // Cloudinary configuration to upload images
 
+import { config } from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
+
+config({ path: './config/config.env' });
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
 });
 
 const uploader = async (file) => {
