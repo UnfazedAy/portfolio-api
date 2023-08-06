9a5fc051d0e164b7e1a67b662ec9047b4ea87b13
UnfazedAy <ayomidesoniyi@gmail.com>
2023-08-06 19:08:33 +0100

    Handled an edge case of missing file

--- controllers/projects.js
+++ controllers/projects.js
@@ -15,16 +15,32 @@ export const createProject = async (req, res, next) => {
       if (err) {
         return res.status(400).json({ success: false, error: err.message });
       }
-      console.log(req.file);
+      // console.log('Printing req.file');
+      // console.log();
+      // console.log(req.file);
+
+      if (!req.file) {
+        return res
+          .status(400)
+          .json({ success: false, error: 'No file uploaded' });
+      }
+
       // Convert the uploaded image to data uri using bufferToDataUri function
       const { mimetype, buffer } = req.file;
+      // console.log('Printing mimetype and buffer');
+      // console.log();
+      // console.log(mimetype, buffer);
+
       const dataUri = bufferToDataUri(mimetype, buffer).content;
+      console.log('Printing dataUri');
+      console.log();
+      console.log(dataUri);
 
       // Upload the image to cloudinary using uploader function
       const imageUrl = await uploader(dataUri);
 
       // Create a new project with the cloudinary uploaded image url
-      const project = await Project.create({ imageUrl, ...req.body });
+      const project = await Project.create({ image: imageUrl, ...req.body });
 
       res.status(201).json({ success: true, data: project });
     });
