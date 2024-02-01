78fda8e7a07aa11081fd69fc3dde5a7d06bd55d8
UnfazedAy <ayomidesoniyi@gmail.com>
2024-02-01 11:56:58 +0100

    Made use of loggers

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import logger from '../logger.js';
 
 const connectDB = async () => {
+  logger.info('Connecting to MongoDB...'.cyan.underline.bold);
+  logger.info(process.env.MONGO_URI);
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
